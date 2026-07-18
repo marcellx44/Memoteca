6f10fbd354eeb22c5904b6d901d5531a86cce82b
Marcella <marcella.oliveira.diasss@gmail.com>
2026-07-18 15:39:49 -0300

    feat: adiciona funções de editar pensamento

--- js/api.js
+++ js/api.js
@@ -25,5 +25,32 @@ export const api ={
         } catch {
             alert('Erro ao salvar pensamentos')
         }
-    }
+    },
+
+    //função de buscar pensamento pelo id
+    async buscarPensamentoPorId(id){
+        try{
+            const resposta = await fetch(`http://localhost:3000/pensamentos/${id}`) 
+            const pensamento = await resposta.json()
+            return pensamento;
+        } catch {
+            alert('Erro ao buscar pensamento por id')
+        }
+    },
+
+     //função de editar pensamentos 
+    async editarPensamento(pensamento){
+        try{
+            const resposta = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(pensamento)
+            })
+            return await resposta.json()
+        } catch {
+            alert('Erro ao editar pensamentos')
+        }
+    },
 }
