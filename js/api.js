30ea7250a4f7f8d4d399c7746244dfe1cc239a73
Marcella <marcella.oliveira.diasss@gmail.com>
2026-07-18 00:20:29 -0300

    feat: adiciona método de salvar na API

--- js/api.js
+++ js/api.js
@@ -10,5 +10,20 @@ export const api ={
         } catch {
             alert('Erro ao buscar pensamentos')
         }
+    },
+    //função de salvar pensamentos 
+    async salvarPensamentos(pensamento){
+        try{
+            const resposta = await fetch('http://localhost:3000/pensamentos', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(pensamento)
+            })
+            return await resposta.json()
+        } catch {
+            alert('Erro ao buscar pensamentos')
+        }
     }
 }
